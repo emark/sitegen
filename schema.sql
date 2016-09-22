bf11cb86935cd1267697376b3527edd20d920685
Maxim Sviridenko <maxim@emrk.ru>
2016-09-22 07:45:23 +0700

    edit schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 drop database if exists sitegen;
 create database sitegen;
 use sitegen;
-create table site (id integer not null auto_increment primary key, url text, meta text, content text);
+create table site (url text, meta text, content text);
 
