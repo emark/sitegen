a5d9047c931bc765a55790293c598972ffc63f31
Maxim Sviridenko <maxim@emrk.ru>
2021-08-18 02:16:20 +0700

    update utils script

--- utils/siteup.sh
+++ utils/siteup.sh
@@ -3,20 +3,23 @@
 cd ~/projects/site/cgi-bin/sitegen/utils/
 
 SITE=~/projects/site/htdocs
-CRONFILE=tmp/cron.txt
+DB_CONNECT=$(cat db.conf)
 
 if [ -f "$CRONFILE" ]; then
+
 git pull
 
 rm $SITE/*.html
 
-#mv $CRONFILE pages/cron.txt
 cd tmp
 
 wget --no-check-certificate --input-file cron.txt
 
 mv -vf *.html $SITE
 rm cron.txt
+
+#Dump database
+mysqldump $DB_CONNECT --result-file=dump/dump.sql 
 
 echo "Updated"
 fi
