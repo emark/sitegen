62434e18c145be159a3445dee3af99a8bb721f60
Maxim Sviridenko <maxim@emrk.ru>
2021-11-26 20:00:32 +0700

    add ctl, tmpl

---
+++ lib/Sitegen/Controller/Arenda.pm
@@ -0,0 +1,27 @@
+package Sitegen::Controller::Arenda;
+use Mojo::Base 'Mojolicious::Controller';
+
+sub rentals(){
+	my $self = shift;
+	my $config = $self->config;
+	my $url = $self->param('url'); 
+	my $path = $config->{'downloads'}.'arenda/';
+
+	my @files = glob ($path."$url-*.*");
+	foreach my $file (@files){
+        $file =~s /$path//;
+    };
+
+	my $media = \@files;
+
+	$self->render(
+		layout => $config->{'site'},
+		template => $config->{'site'}.'/rentals',
+		format => 'html',
+		url => $url,
+		page => {'url' => $url},
+		media => $media,
+	);
+};
+
+1;
