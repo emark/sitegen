e7f05ffe5a1601dd1da994ab829cad9ef5f43dc4
Maxim Sviridenko <sviridenko.maxim@gmail.com>
2026-03-14 18:24:36 +0700

    Added notification script
    	Sends email notification using api email-gate

---
+++ utils/extends/notify.pl
@@ -0,0 +1,114 @@
+#!/usr/bin/env perl
+# Send notification for new calls 
+# MAXIM SVIRIDENKO (), sviridenko.maxim@gmail.com
+
+use strict;
+use warnings;
+use utf8;
+use DBIx::Custom;
+use Mojo::UserAgent;
+use Mojo::JSON qw(from_json);
+use Data::Dumper;
+
+my $cfile = 'app.conf';
+my %config;
+print "Loading config...";
+
+open (CONF, "<", $cfile) || die "Can't load config from file: $cfile";
+	while(<CONF>){
+		chop $_;
+		(my $key, my $val) = split('#', $_);
+		$config{$key} = $val;
+	};
+	print "ok\n";
+close CONF;
+
+&GetCalls;
+
+sub GetCalls{
+	print "Connect to database...";
+	my $dbi = DBIx::Custom->connect( 
+            dsn => $config{dsn}, 
+            user => $config{dbuser}, 
+            password => $config{dbpassword}, 
+            option => {sqlite_unicode => 1} 
+    );
+	print "ok\nChecking for new calls...";
+
+	my $calls = $dbi->select(
+		table => $config{'table'},
+		where => { 
+			read => 0,
+			notify => 0,
+		},
+	)->fetch_all;
+
+	print "ok\nGet ".@$calls." new calls\n";
+	my @call_id = &SendNotification($calls) if @$calls;
+	
+	if (@call_id){
+		print "Update notify flag...";
+		$dbi->update(
+        	{notify => 1},
+            table => $config{'table'},
+            where => {id => \@call_id}
+        );
+		print "ok";
+	};
+};
+
+sub SendNotification{
+	my $calls = $_[0];
+	my @call_id;
+	my $text = "Hello,\nGet ".@$calls." new call(s)\n";
+
+	foreach my $key ( @{$calls} ){
+		push (@call_id, $key->[0]);
+		my $c = from_json($key->[1]);
+		$text = $text."\n---\nName: $c->{'customer_name'}\nContacts: $c->{'customer_phone'}, $c->{'customer_email'}\nGuests: $c->{'guests_num'} Date From: $c->{'from_date'} $c->{'from_time'} Date Till: $c->{'till_date'} $c->{'till_time'}\nComment: $c->{'booking_comment'}\n";
+		if ($c->{'service'}){
+			$text = $text."AdSvc: ";
+			if(ref($c->{'service'}) eq 'ARRAY'){
+				$text = $text."@{$c->{'service'}}\n";
+			}else{
+				$text = $text.$c->{'service'}."\n";
+			};
+		};
+	};
+
+	my @api_gate = (
+		'https://api-reserve.msndr.net/v1/email/messages',
+		'https://app.smsgold.ru/v1/email/messages',
+	);
+
+	my $result = '';
+
+	while (<@api_gate>){
+		
+		my $api = $_;
+		if ($result ne 'Created'){
+
+			print "Sending message to $config{'rcpt'}.\nAPI gate: $api\nStatus: ";
+
+			my $ua = Mojo::UserAgent->new;
+			my $tx = $ua->post($api,
+			{
+				'Authorization' => 'Bearer '.$config{'token'},
+				'Accept' => '*/*',
+			} => json => {
+						from_email => $config{'sender'},
+						from_name => $config{'sender_name'},
+						to => $config{'rcpt'},
+						subject => $config{'subject'},
+						text => $text,
+						}
+			)->result;
+
+			$result = $tx->message;
+			print $result."\n";
+		};
+	};
+
+	@call_id = () if $result ne 'Created';
+	return @call_id;
+};
